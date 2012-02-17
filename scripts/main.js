a7b91d7cae9828db5c8b208f278a295c739a3323
Alex Buchanan <buchanae@gmail.com>
2012-02-17 02:01:59 -0800

    add some junk for animating along a path

--- scripts/main.js
+++ scripts/main.js
@@ -35,8 +35,32 @@ define(['domReady', 'Point', 'IKChain', 'raphael'], function(domReady, Point, IK
       var ik_rarm = new IKChain( d.rarm );
       var target = new Point( 170, 40 );
 
+      var inc = -1;
+      /*
+      setInterval(function() {
+          if( target.x < 10 || target.x > 170 ) {
+              inc = inc * -1;
+          }
+          target.x += inc;
+          c.attr('cx', target.x);
+          ik_rarm.solve(target);
+          update_arm();
+      }, 10);
+      */
+      var p = paper.path("M 60, 10 L 90, 10 L 60, 60");
+      var len = p.getTotalLength();
+
+      paper.customAttributes.along = function( v ){
+          var point = p.getPointAtLength(v * len);
+          return {
+              cx: point.x,
+              cy: point.y
+          };
+      };
+
       var c = paper.circle( target.x, target.y , 2 );
       c.attr('fill', 'red');
+
       c.drag(function(dx, dy, x, y){
           c.attr('cx', x);
           c.attr('cy', y);
@@ -45,6 +69,23 @@ define(['domReady', 'Point', 'IKChain', 'raphael'], function(domReady, Point, IK
           ik_rarm.solve(target);
           update_arm();
       });
+
+      c.attr({along: 0});
+      c.onAnimation(function() { 
+          target.x = this.attr('cx');
+          target.y = this.attr('cy');
+          ik_rarm.solve(target);
+          update_arm();
+      });
+
+      var backward = Raphael.animation({ along: 0 }, 1000, function() {
+          c.animate(forward);
+      });
+      var forward = Raphael.animation({ along: 1 }, 1000, function() {
+          c.animate(backward);
+      });
+
+      c.animate(forward);
 
       // shoulder mark
       var s = paper.circle( 100, 30, 2 );
