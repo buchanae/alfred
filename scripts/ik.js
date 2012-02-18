9d24194cce4d5cf2ed46492e7cbcda3a87b83016
Alex Buchanan <buchanae@gmail.com>
2012-02-18 15:55:27 -0800

    paper.js is hawt

---
+++ scripts/ik.js
@@ -0,0 +1,64 @@
+define(function(){
+
+    var ALLOWED_ITERATIONS = 10;
+    var TOLERANCE = 0.1;
+
+    return function( path, target ){
+
+        if ( path.firstSegment.point.getDistance(target) <= path.length ) {
+
+
+            var iteration = 0;
+
+            // TODO tolerance checking is wrong.  should be checking the amount
+            // that the end point moved during the last iteration.  If it moved
+            // above a tolerance, keep moving.  If it barely moved, it is possible
+            // that the chain is constrained from moving anymore and we should stop.
+            while( !path.lastSegment.point.isClose( target, TOLERANCE )
+                   && iteration < ALLOWED_ITERATIONS ){
+
+
+                var root = path.firstSegment.point.clone();
+
+                var t = target;
+                for( var s = path.lastSegment.previous; s !== null; s = s.previous ){
+
+                    var d = s.point.getDistance(s.next.point);
+                    s.next.point = t;
+                    var a = s.point.subtract(s.next.point).normalize(d);
+                    t = s.next.point.add(a);
+                }
+                path.firstSegment.point = t;
+
+                t = root;
+                path.reverse();
+
+                for( var s = path.lastSegment.previous; s !== null; s = s.previous ){
+
+                    var d = s.point.getDistance(s.next.point);
+                    s.next.point = t;
+                    var a = s.point.subtract(s.next.point).normalize(d);
+                    t = s.next.point.add(a);
+                }
+                path.firstSegment.point = t;
+
+                path.reverse();
+                iteration++;
+            }
+
+        } else {
+
+            var v = target.subtract(path.firstSegment.point);
+            var t = path.firstSegment.point;
+
+            for( var s = path.firstSegment.next; s !== null; s = s.next ){
+
+                var d = s.point.getDistance(s.previous.point);
+                s.previous.point = t;
+                var a = v.normalize(d);
+                t = s.previous.point.add(a);
+            }
+            path.lastSegment.point = t;
+        }
+    };
+});
--- scripts/main.js
+++ scripts/main.js
@@ -1,94 +1,30 @@
-define(['domReady', 'Point', 'IKChain', 'raphael'], function(domReady, Point, IKChain) {
+define(['domReady', 'ik', 'paper'], function(domReady, ik) {
 
   domReady(function() {
-      var paper = Raphael(document.getElementById("test"), 200, 200);
+      var canvas = document.getElementById('canvas');
+      paper.setup(canvas);
 
-      function Dude() {
-          var shoulder = new Point(100, 30);
-          var hip = new Point(100, 70);
 
-          this.rarm = [shoulder, new Point(120, 30), new Point(140, 30)];
-          this.larm = [shoulder, new Point(80, 30), new Point(60, 30)];
-          
-          this.rleg = [hip, new Point(120, 70), new Point(140, 70)];
-          this.lleg = [hip, new Point(80, 70), new Point(60, 70)];
+      var path = new paper.Path();
+      path.strokeColor = 'black';
+      path.add([10, 40], [50, 40], [90, 40]);
+
+
+      var a = new paper.Point(25, 30);
+
+      ik( path, a );
+
+      var c = new paper.Path.Circle(a, 2);
+      c.fillColor = 'red';
+
+      var tool = new paper.Tool();
+      tool.onMouseMove = function( e ){
+          ik( path, e.point );
+          c.position = e.point;
       }
 
-      var d = new Dude();
-
-      var arm_dots = [];
-      for( var i = 0; i < d.rarm.length; i++ ){
-
-          var c = paper.circle( d.rarm[i].x, d.rarm[i].y, 3 );
-          c.attr('fill', 'green');
-          arm_dots.push(c);
+      var maxPoint = new paper.Point( 200, 200 );
+      paper.view.onFrame = function() {
       }
-
-      function update_arm() {
-          for( var i = 0; i < d.rarm.length; i++ ){
-
-              arm_dots[i].attr('cx', d.rarm[i].x);
-              arm_dots[i].attr('cy', d.rarm[i].y);
-          }
-      }
-
-      var ik_rarm = new IKChain( d.rarm );
-      var target = new Point( 170, 40 );
-
-      var inc = -1;
-      /*
-      setInterval(function() {
-          if( target.x < 10 || target.x > 170 ) {
-              inc = inc * -1;
-          }
-          target.x += inc;
-          c.attr('cx', target.x);
-          ik_rarm.solve(target);
-          update_arm();
-      }, 10);
-      */
-      var p = paper.path("M 60, 10 L 90, 10 L 60, 60");
-      var len = p.getTotalLength();
-
-      paper.customAttributes.along = function( v ){
-          var point = p.getPointAtLength(v * len);
-          return {
-              cx: point.x,
-              cy: point.y
-          };
-      };
-
-      var c = paper.circle( target.x, target.y , 2 );
-      c.attr('fill', 'red');
-
-      c.drag(function(dx, dy, x, y){
-          c.attr('cx', x);
-          c.attr('cy', y);
-          target.x = x;
-          target.y = y;
-          ik_rarm.solve(target);
-          update_arm();
-      });
-
-      c.attr({along: 0});
-      c.onAnimation(function() { 
-          target.x = this.attr('cx');
-          target.y = this.attr('cy');
-          ik_rarm.solve(target);
-          update_arm();
-      });
-
-      var backward = Raphael.animation({ along: 0 }, 1000, function() {
-          c.animate(forward);
-      });
-      var forward = Raphael.animation({ along: 1 }, 1000, function() {
-          c.animate(backward);
-      });
-
-      c.animate(forward);
-
-      // shoulder mark
-      var s = paper.circle( 100, 30, 2 );
-      s.attr('fill', 'red');
   });
 });
